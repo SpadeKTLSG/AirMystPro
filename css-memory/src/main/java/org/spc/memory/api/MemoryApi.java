9e0a473bbc6cea102038a8826cf59e4c51edfeb1
caowenxing <caowenxing@newland.com.cn>
2025-01-12 23:55:10 +0800

    进程部分搬运+Feign调用处理2

--- css-memory/src/main/java/org/spc/memory/api/MemoryApi.java
+++ css-memory/src/main/java/org/spc/memory/api/MemoryApi.java
@@ -10,5 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/memory")
 @RequiredArgsConstructor
-public class MemoryAPI {
+public class MemoryApi {
 }
