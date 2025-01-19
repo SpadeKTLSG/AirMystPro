e8486d030318450be43e080eac98c80f6644b479
caowenxing <caowenxing@newland.com.cn>
2025-01-19 16:49:34 +0800

    全新优化体系 完成, 开始具体功能点转化

--- css-base/src/main/java/org/spc/base/artifact/BaseArtifact.java
+++ css-base/src/main/java/org/spc/base/artifact/BaseArtifact.java
@@ -1,6 +1,8 @@
 package org.spc.base.artifact;
 
 import lombok.extern.slf4j.Slf4j;
+import org.spc.api.IHamamap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,6 +11,15 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public abstract class BaseArtifact {
+
+    //! Properties
+
+    /**
+     * 动态配置器
+     */
+    @Autowired
+    IHamamap<String, String> dynamicConfiger;
+
 
     //! BaseArtifact Functions
 
@@ -19,6 +30,11 @@ public abstract class BaseArtifact {
         log.debug("Class: {} Instance: {} is loading", clazz, instance);
         loadConfig();
     }
+
+    /**
+     * 初始化0 子类实现
+     */
+    public abstract void initial();
 
     /**
      * 初始化2 加载配置
--- css-base/src/main/java/org/spc/base/artifact/CoreArtifact.java
+++ css-base/src/main/java/org/spc/base/artifact/CoreArtifact.java
@@ -1,18 +1,28 @@
 package org.spc.base.artifact;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 /**
  * 核心工件
  */
+@Slf4j
+@Service
 public class CoreArtifact extends BaseArtifact {
 
+    /**
+     * 具体工件加载器
+     */
     public void initial() {
         Class<?> clazz = this.getClass();
         Object instance = this;
         super.initial(clazz, instance);
     }
 
+    /**
+     * 自定义加载配制处理
+     */
     @Override
     public void loadConfig() {
-
     }
 }
