89c3cb087155ea4d93949f771d1f6d89419de349
caowenxing <caowenxing@newland.com.cn>
2025-01-21 11:51:49 +0800

    攻坚3

--- css-file/src/main/java/org/spc/file/app/FileSyS.java
+++ css-file/src/main/java/org/spc/file/app/FileSyS.java
@@ -2,14 +2,20 @@ package org.spc.file.app;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
 import org.spc.base.app.BaseApp;
 import org.spc.base.common.enumeration.FileDirTYPE;
 import org.spc.base.common.enumeration.ROOT_PATH;
+import org.spc.base.common.exception.FailedException;
 import org.spc.base.entity.file.TREE;
 import org.spc.base.entity.file.dir;
 import org.spc.base.entity.file.file;
 import org.spc.base.entity.file.struct.FCB;
 import org.spc.base.entity.file.struct.node;
+import org.spc.file.compo.HandleDiskCompo;
+import org.spc.file.compo.HandleFileCompo;
+import org.spc.file.special.TXTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -18,15 +24,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import static org.spc.base.common.constant.FileCT.*;
+import static org.spc.base.common.util.ByteUtil.byte2Str;
+
 
 /**
  * 文件应用
  */
+@Slf4j
 @Service
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class FileSyS extends BaseApp {
 
+    @Autowired
+    TXTUtil txtUtil;
+
+    @Autowired
+    HandleDiskCompo handleDiskCompo;
 
     /**
      * 文件系统树形结构
@@ -48,6 +63,9 @@ public class FileSyS extends BaseApp {
 
 
     //All Compos
+
+    @Autowired
+    HandleFileCompo handleFileCompo;
 
 
     //? Default Methods
@@ -94,7 +112,7 @@ public class FileSyS extends BaseApp {
      * ?正常模式
      * <p>正常从磁盘完全刷新文件系统</p>
      */
-    public static void normalRebootFile() {
+    public void normalRebootFile() {
         //需要从磁盘读取当前文件树信息, 在基础索引树的基础上重建
         //因为设置了一个块里只能有一个文件(整个/部分), 因此需要按照FAT的顺序遍历磁盘, 读取每个文件对象(file/dir)的字节流, 转换为对象, 挂载到树上
 
@@ -103,12 +121,13 @@ public class FileSyS extends BaseApp {
         setDefaultEM();
 
         //?使用order序列遍历查找对应文件/文件夹的内容, 逐个添加到树上, 同时加到PM上
-        List<Integer> orderList = getFATOrder();
+        List<Integer> orderList = handleDiskCompo.getFATOrder();
 
         if (orderList != null && orderList.isEmpty()) {
             return;
         }
 
+        assert orderList != null;
         for (Integer single : orderList) {
 
             //去除FAT和DEFAULT位置盘块
@@ -116,14 +135,14 @@ public class FileSyS extends BaseApp {
                 continue;
 
             //读取出块切分为FCB_LENGTH和余下部分
-            byte[] tempBytes = read1Block2Bytes(single);
+            byte[] tempBytes = handleDiskCompo.getBlockArtifact().read1Block2Bytes(single);
             byte[] fcbBytes = new byte[FCB_BYTE_LENGTH];
             byte[] contentBytes = new byte[BLOCK_SIZE - FCB_BYTE_LENGTH];
             System.arraycopy(tempBytes, 0, fcbBytes, 0, FCB_BYTE_LENGTH);
             System.arraycopy(tempBytes, FCB_BYTE_LENGTH, contentBytes, 0, BLOCK_SIZE - FCB_BYTE_LENGTH);
 
             //提取出fcb和Contents
-            FCB tempFCB = bytes2Fcb_AppendPM(fcbBytes); //这里有当前无法实现的重大BUG: 文件系统始终是JAVA对象,重启后无法复原. 只能进行转移
+            FCB tempFCB = handleFileCompo.getFileArtifact().bytes2Fcb_AppendPM(fcbBytes); //这里有当前无法实现的重大BUG: 文件系统始终是JAVA对象,重启后无法复原. 只能进行转移
             String tempContent = byte2Str(contentBytes);
 
             //添加封装的对象到TR(FS)
@@ -140,7 +159,7 @@ public class FileSyS extends BaseApp {
      * <p>直接用当前JAVA文件对象结构重建系统</p>
      * <p>根据当前Java对象的挂载关系操作</p>
      */
-    public static void coverRebootFile() {
+    public void coverRebootFile() {
         //由于超纲, 未实现
 
         log.debug("文件模块覆盖完成");
@@ -153,9 +172,9 @@ public class FileSyS extends BaseApp {
      *
      * @comment 你最好不要玩火...
      */
-    public static void cleanRebootFile() {
-
-        FileApp.fileSyS = initialFileSys();
+    public void cleanRebootFile() {
+        //拒绝实现
+        //FileApp.fileSyS = initialFileSys();
         log.debug("文件模块格式化完成");
     }
 
@@ -166,7 +185,7 @@ public class FileSyS extends BaseApp {
     /**
      * 获取初始TR
      */
-    public static TREE initialTR() {
+    public TREE initialTR() {
         TREE tree = new TREE();
         tree.name = FILE_TREE_NAME;
         tree.root = new node(ROOT_AUTH); //挂载根节点
@@ -180,7 +199,7 @@ public class FileSyS extends BaseApp {
      *
      * @param root 根节点
      */
-    public static void setDefaultTR(node root) {
+    public void setDefaultTR(node root) {
 
         boolean isFirst = true;
 
@@ -208,9 +227,9 @@ public class FileSyS extends BaseApp {
      *
      * @param fcb 新增对象的FCB
      */
-    public static void addTR(FCB fcb) { //fcb或者是node皆可
+    public void addTR(FCB fcb) { //fcb或者是node皆可
         //? 拿到FCB后, 通过String切分判断其上级已存在node的位置, 封装到searchUpperNode方法中
-        node dir_temp = searchUpperNode(fcb);
+        node dir_temp = handleFileCompo.searchUpperNode(fcb);
 
         if (dir_temp == null) {
             return;
@@ -238,9 +257,9 @@ public class FileSyS extends BaseApp {
      *
      * @param fcb 删除对象的FCB
      */
-    public static void deleteTR(FCB fcb) {
-        node parentNode = searchUpperNode(fcb); //得到要删除的节点的父节点(一定是目录)
-        node targetNode = selectTR2Node(fcb);//得到要删除的节点本身
+    public void deleteTR(FCB fcb) {
+        node parentNode = handleFileCompo.searchUpperNode(fcb); //得到要删除的节点的父节点(一定是目录)
+        node targetNode = handleFileCompo.selectTR2Node(fcb);//得到要删除的节点本身
 
         if (parentNode == null) {
             return;
@@ -252,8 +271,7 @@ public class FileSyS extends BaseApp {
 
         if (Objects.equals(parentNode.fcb.getPathName(), "/:")) { //安全判断
             log.warn("根目录下的8个节点不能删除!");
-            alertUser("小子, 你在玩火!");
-            return;
+            throw new FailedException("小子, 你在玩火!");
         }
 
         // 如果要删除的节点是其父节点的左孩子
@@ -277,16 +295,15 @@ public class FileSyS extends BaseApp {
 
 
     //修改TR节点
-    public static void alterTR(FCB fcb1, FCB fcb2) {
+    public void alterTR(FCB fcb1, FCB fcb2) {
         //? 拿到FCB1后, 通过String切分判断其位置, 定位到地点后执行孩子兄弟树的修改节点操作
 
         if (Objects.equals(fcb1.getPathName(), "/:")) { //安全判断
             log.warn("根目录下的8个节点不能删除!");
-            alertUser("小子, 你在玩火!");
-            return;
+            throw new FailedException("小子, 你在玩火!");
         }
 
-        Objects.requireNonNull(selectTR2Node(fcb1)).fcb = fcb2;//直接替换内容
+        Objects.requireNonNull(handleFileCompo.selectTR2Node(fcb1)).fcb = fcb2;//直接替换内容
 
     }
 
@@ -298,8 +315,8 @@ public class FileSyS extends BaseApp {
      * @return 返回对应的路径
      * @deprecated
      */
-    public static String selectTR2Path(FCB fcb) {
-        return pathTR(selectTR2Node(fcb));
+    public String selectTR2Path(FCB fcb) {
+        return handleFileCompo.pathTR(handleFileCompo.selectTR2Node(fcb));
     }
 
 
@@ -309,9 +326,9 @@ public class FileSyS extends BaseApp {
      * @param fcb 给定的fcb
      * @return 返回对应的文件/文件夹对象
      */
-    public static Object selectTR2Content(FCB fcb) {
-        FCB originalFcb = Objects.requireNonNull(selectTR2Node(fcb)).fcb;
-        String allBlock = read1BlockiTXT(originalFcb.getStartBlock());
+    public Object selectTR2Content(FCB fcb) {
+        FCB originalFcb = Objects.requireNonNull(handleFileCompo.selectTR2Node(fcb)).fcb;
+        String allBlock = txtUtil.read1BlockiTXT(originalFcb.getStartBlock());
         String[] temp = allBlock.split(" ");
         StringBuilder sb = new StringBuilder();
         int lastMeaningfulIndex = 0;
@@ -335,7 +352,7 @@ public class FileSyS extends BaseApp {
     /**
      * 获取初始PM
      */
-    public static Map<Integer, String> initialPM() {
+    public Map<Integer, String> initialPM() {
         Map<Integer, String> pm = new HashMap<>();
         for (int i = 0; i < 1000; i++) pm.put(i, "");
         pm.put(0, "/"); //根节点
@@ -349,10 +366,10 @@ public class FileSyS extends BaseApp {
      *
      * @deprecated 已经通过磁盘模块初始化完成
      */
-    public static void setDefaultPM() {
-        fileSyS.pathManager = initialPM();
+    public void setDefaultPM() {
+        pathManager = initialPM();
         for (ROOT_PATH root_path : ROOT_PATH.values())
-            bindPM(str2Path(String.valueOf(root_path)));
+            bindPM(handleFileCompo.getFileArtifact().str2Path(String.valueOf(root_path)));
     }
 
 
@@ -362,15 +379,15 @@ public class FileSyS extends BaseApp {
      * @param pathName FCB的PathName
      * @comment 这样硬盘只需要存储对应的键即可
      */
-    public static Integer bindPM(String pathName) {
+    public Integer bindPM(String pathName) {
 
-        List<Integer> keys = fileSyS.pathManager.entrySet().stream() //将Map转换为Stream，过滤出值等于目标值的键值对，映射为键，收集为集合
+        List<Integer> keys = pathManager.entrySet().stream() //将Map转换为Stream，过滤出值等于目标值的键值对，映射为键，收集为集合
                 .filter(entry -> entry.getValue().isEmpty())
                 .map(Map.Entry::getKey)
                 .toList();
 
         //log.debug("当前路径映射器中的空白位置: {}", keys.size());
-        fileSyS.pathManager.put(keys.get(0), pathName);
+        pathManager.put(keys.get(0), pathName);
         return keys.get(0);
     }
 
@@ -381,7 +398,7 @@ public class FileSyS extends BaseApp {
      * @param pathName FCB的PathName
      * @return 路径管理器中的键
      */
-    public static Integer deletePM(String pathName) {
+    public Integer deletePM(String pathName) {
         //!合法性校验: 禁止删除根目录和根目录下的节点
 
         if (pathName.equals("/")) {
@@ -396,7 +413,7 @@ public class FileSyS extends BaseApp {
             }
         }
 
-        List<Integer> keys = fileSyS.pathManager.entrySet().stream() //将Map转换为Stream，过滤出值等于目标值的键值对，映射为键，收集为集合
+        List<Integer> keys = pathManager.entrySet().stream() //将Map转换为Stream，过滤出值等于目标值的键值对，映射为键，收集为集合
                 .filter(entry -> entry.getValue().equals(pathName))
                 .map(Map.Entry::getKey)
                 .toList();
@@ -406,7 +423,7 @@ public class FileSyS extends BaseApp {
             return null;
         }
 
-        fileSyS.pathManager.put(keys.get(0), "");
+        pathManager.put(keys.get(0), "");
         return keys.get(0);
     }
 
@@ -419,7 +436,7 @@ public class FileSyS extends BaseApp {
      * @param pathName_new 新PathName
      * @return 路径管理器中的键
      */
-    public static Integer alterPM(String pathName_or, String pathName_new) {
+    public Integer alterPM(String pathName_or, String pathName_new) {
         deletePM(pathName_or);
         return bindPM(pathName_new);
     }
@@ -431,8 +448,8 @@ public class FileSyS extends BaseApp {
      * @param key 路径管理器中的键
      * @return FCB的PathName
      */
-    public static String selectPM(Integer key) {
-        return fileSyS.pathManager.get(key);
+    public String selectPM(Integer key) {
+        return pathManager.get(key);
     }
 
 
@@ -443,7 +460,7 @@ public class FileSyS extends BaseApp {
      * 获取扩展名管理器
      * <p>初始化为0-100的Integer与空String键值对</p>
      */
-    public static Map<Integer, String> initialEM() {
+    public Map<Integer, String> initialEM() {
         Map<Integer, String> em = new HashMap<>();
         for (int i = 0; i < 100; i++) em.put(i, "");
         return em;
@@ -454,18 +471,18 @@ public class FileSyS extends BaseApp {
      * 初始化EM
      * <p>按照文档和文件的扩展名进行设置</p>
      */
-    public static void setDefaultEM() {
-        fileSyS.extendManager = initialEM();
+    public void setDefaultEM() {
+        extendManager = initialEM();
         try {//系统自带的直接指定提高效率
             int i = 0;
             for (; i < DIR_EXTEND.size(); i++) {
-                fileSyS.extendManager.put(i, DIR_EXTEND.get(i));
+                extendManager.put(i, DIR_EXTEND.get(i));
             }
             for (int k = 0; k < FILE_EXTEND.size(); k++) {
-                fileSyS.extendManager.put(i + k, FILE_EXTEND.get(k));
+                extendManager.put(i + k, FILE_EXTEND.get(k));
             }
             for (String s : AddedEXTEND) {
-                bindEM(s); //同步用户添加的扩展名
+                handleFileCompo.bindEM(s); //同步用户添加的扩展名
             }
         } catch (Exception e) {
             log.warn("装不下了, extendManager被撑爆咯! {}", e.getMessage());
@@ -480,7 +497,7 @@ public class FileSyS extends BaseApp {
      *
      * @param A 文件/文件夹对象
      */
-    public static void addContentFS(Object A) {
+    public void addContentFS(Object A) {
         //?自动绑定PM和EM, Path名字和扩展名都会在创建磁盘系统创建文件FCB字节对象时候自动绑定 (这叫解耦? SpadeK?)
         //?那么文件系统剩下的工作就是加入TR了
         //? TR新增节点
@@ -506,7 +523,7 @@ public class FileSyS extends BaseApp {
      *
      * @param A 文件/文件夹对象
      */
-    public static void deleteContentFS(Object A) {
+    public void deleteContentFS(Object A) {
 
         //?需要解除PM的绑定(通过磁盘系统绑定)
 //        deletePM(A);
@@ -537,7 +554,7 @@ public class FileSyS extends BaseApp {
      * @param A 被修改的对象
      * @param B 修改后的对象
      */
-    public static void alterContentFS(Object A, Object B) {
+    public void alterContentFS(Object A, Object B) {
 
         //?鉴权 * 2
         if (!(A instanceof file) & !(A instanceof dir)) {
@@ -585,7 +602,7 @@ public class FileSyS extends BaseApp {
      * @param A 文件/文件夹对象
      * @return 文件模块的TR生成的拷贝镜像对象
      */
-    public static Object selectContentFS(Object A) {
+    public Object selectContentFS(Object A) {
         if (A instanceof file file_temp) {
             log.debug("正在往文件树查询文件索引{}", file_temp.fcb.getPathName());
             return selectTR2Content(file_temp.fcb);
